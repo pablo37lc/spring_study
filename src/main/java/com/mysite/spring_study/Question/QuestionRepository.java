129317854827fdcb379c4755e76f2a7ed5e31083
pablo37lc <pablo37lc@gmail.com>
2023-08-31 13:48:28 +0900

    add search

--- src/main/java/com/mysite/spring_study/Question/QuestionRepository.java
+++ src/main/java/com/mysite/spring_study/Question/QuestionRepository.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
@@ -15,5 +17,19 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     List<Question> findBySubjectLike(String subject);
 
     Page<Question> findAll(Pageable pageable);
+
+    @Query("select "
+            + "distinct q "
+            + "from Question q "
+            + "     left outer join SiteUser uq on q.author=uq "
+            + "     left outer join Answer a on a.question=q "
+            + "     left outer join SiteUser ua on a.author=ua "
+            + "where "
+            + "      q.subject like %:kw% "
+            + "   or q.content like %:kw% "
+            + "   or uq.username like %:kw% "
+            + "   or a.content like %:kw% "
+            + "   or ua.username like %:kw% ")
+    Page<Question> findAllByKeyword(@Param("keyword") String keyword, Pageable pageable);
 
 }
